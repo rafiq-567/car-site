02504506de5740c07dadd7d0f47da0768c379637
unknown <mdrafiqulakhter0@gmail.com>
2026-03-31 11:46:19 +0600

    added the navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className='flex justify-center'>
+                <ul className='flex justify-between w-1/2'>
+                    <li>Home</li>
+                    <li>Browse Cars</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
