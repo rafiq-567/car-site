cf00f52abbdc3a8a29fe263a43e56b31ca294b65
unknown <mdrafiqulakhter0@gmail.com>
2026-04-01 02:27:17 +0600

    final review

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,14 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body>
       <Navbar></Navbar>
-        <main className="h-screen h-max-[600px]">
+        <main>
           {children}
         </main>
-        
         </body>
     </html>
   );
